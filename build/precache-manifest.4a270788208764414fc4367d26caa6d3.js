d292e763c24f6ca7dfd8a830aa0fcfc4b4ebd479
naresh <nareshkmr219@gmail.com>
2020-08-27 12:23:22 +0530

    first commit

---
+++ build/precache-manifest.4a270788208764414fc4367d26caa6d3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913e47a0039ab2c9c958152c9e4fae10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a47108ef8d6e77b0b6e4",
+    "url": "/static/css/2.7e458a04.chunk.css"
+  },
+  {
+    "revision": "a690acf70725c44e4887",
+    "url": "/static/css/main.8c454b25.chunk.css"
+  },
+  {
+    "revision": "a47108ef8d6e77b0b6e4",
+    "url": "/static/js/2.fd7ab8ac.chunk.js"
+  },
+  {
+    "revision": "af7bbd2fb030999335af9e46760681f5",
+    "url": "/static/js/2.fd7ab8ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a690acf70725c44e4887",
+    "url": "/static/js/main.5741a8dd.chunk.js"
+  },
+  {
+    "revision": "0ac5f9323520964b1f89",
+    "url": "/static/js/runtime-main.9aafa8a2.js"
+  },
+  {
+    "revision": "01e205d19c0a37d39e6764741e9c8f48",
+    "url": "/static/media/jothi.01e205d1.png"
+  },
+  {
+    "revision": "c9794425eeb0e2932ae6cd01020241b2",
+    "url": "/static/media/noDp.c9794425.png"
+  },
+  {
+    "revision": "1d44915730f239fc169410115c5108f7",
+    "url": "/static/media/rudrappa.1d449157.png"
+  },
+  {
+    "revision": "5caae76cc7aca9f379462b52f29952d0",
+    "url": "/static/media/vatsala.5caae76c.png"
+  }
+]);
